291d25d6bb61f6cc2e60e451a306d4d2f405e6d8
19ortiz93 <101433810+19ortiz93@users.noreply.github.com>
2022-10-27 23:24:52 -0500

    Add files via upload
    clase 20

--- MOKEPON/js/mokepon.js
+++ MOKEPON/js/mokepon.js
@@ -1,5 +1,39 @@
-function seleccionarMascotaJugador() {
-    alert('SELECCIONASTE TU MASCOTA')
+function iniciarJuego() {
+    let botonMascotaJugador = document.getElementById('boton-mascota')
+    botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
 }
-let botonMascotaJugador = document.getElementById('boton-mascota')
-botonMascotaJugador.addEventListener('click', seleccionarMascotaJugador)
+
+function seleccionarMascotaJugador() {
+    let inputHipodoge = document.getElementById('hipodoge')
+    let inputCapipepo = document.getElementById('capipepo')
+    let inputRatigueya = document.getElementById('ratigueya')
+
+    if (inputHipodoge.checked) {
+        alert('seleccionaste a Hipodoge')
+    } else if (inputCapipepo.checked) {
+        alert('seleccionaste a Capipepo')
+    } else if (inputRatigueya.checked) {
+        alert('seleccionaste a Ratigueya')
+    } else {
+        alert('selecciona una mascota')
+    }
+}
+
+window.addEventListener('load', iniciarJuego)
+
+
+/*<label for="langostelvis">Langostelvis</label>
+<input type="radio" name="mascota" id="langostelvis"/>
+
+<label for="tucapalma">Tucapalma</label>
+<input type="radio" name="mascota" id="tucapalma"/>
+
+<label for="pydos">Pydos</label>
+<input type="radio" name="mascota" id="pydos"/>
+
+Hipodoge -> Agua
+Capipepo -> Tierra
+Ratigueya -> Fuego
+Langostelvis -> Agua y Fuego
+Tucapalma -> Agua y Tierra
+Pydos -> Tierra y Fuego*/
